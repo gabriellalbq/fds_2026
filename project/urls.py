f776083543b092c4e3774e04101513987f0c5ed8
Gabriella Lima <gabriellalimaalb@gmail.com>
2026-09-09 11:07:23 -0300

    Novas atualizações

--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include("hello.urls"))
+    path('hello/', include("hello.urls")),
+    path('forum/', include('forum.urls')),
+    path('', RedirectView.as_view(url='/forum/', permanent=False), name='home'), # Adicionado
 ]
